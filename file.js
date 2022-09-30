846da0098b2a3cb7802570ce645d0f3f761334aa
rehanejaz78 <rehanejaz78@gmail.com>
2022-09-30 20:23:51 +0500

    initial files

---
+++ file.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// var name = function (from, text = "the previous") {
+//   let ac = 10;
+//   return ac;
+// };
+// console.log(name("rehan"));
+
+// name2(24);
+// function name2(num) {
+//   return num;
+// }
+// console.log(name2(23));
+
+//call back functions
+
+function ask(question, yes, no) {
+  if (window.confirm(question)) {
+    yes();
+  } else {
+    no();
+  }
+}
+
+function showYes() {
+  console.log("You agreed");
+  alert("hello rehan !!");
+}
+function showCancel() {
+  console.log("You canceled");
+}
+
+ask("shall we proceed", showYes, showCancel);
